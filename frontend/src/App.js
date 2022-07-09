f7ee42806ca68f82a167699f7a21f32de52fedc8
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-09 11:06:53 -0500

    basic components and routes set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Navbar from './components/Navbar'
+import DisplayFoodList from './components/DisplayFoodList'
+import EditFood from './components/EditFood'
+import AddFood from './components/AddFood'
+import AddUser from './components/AddUser'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+    <>
+    <Router>
+      <Navbar />
+      <br />
+      <Routes>
+        <Route path='/' exact element={<DisplayFoodList/>} />
+        <Route path='/edit:id' exact element={<EditFood/>} />
+        <Route path='/create' exact element={<AddFood/>} />
+        <Route path='/user' exact element={<AddUser/>} />
+      </Routes>
+    </Router>
+    </>
+
+  )
 }
 
 export default App;
