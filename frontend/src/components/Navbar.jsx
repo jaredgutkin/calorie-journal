c82450443c12d26a364ed7b749b8d004dd09421e
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-09 11:37:44 -0500

    playing around with nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,6 +9,20 @@ function Navbar() {
             <Link to='/'>
                 <FaAppleAlt/>
             </Link>
+            <Link className='navbar-brand' to='/'>
+                <FaAppleAlt/>
+            </Link>
+            <div className='collapse navbar-collapse'>
+                <ul className="navbar-nav m1-auto">
+                    <li className='nav.item'>
+                        <Link className='nav-link' to='/'>
+                            <button type='button' className="btn btn-info">
+                                Calorie Info
+                            </button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
     </>
